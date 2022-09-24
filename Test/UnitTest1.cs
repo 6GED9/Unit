4dc9614e83526945d8810f231f3664fff257ba59
wudisluv <vlad.kornilov2015@gmail.com>
2022-09-24 15:48:13 +0300

    Unit

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace HelloWorld;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Math()
+    {
+        int a = 2;
+        int b = 3;
+        int expected = 8;
+        HelloWorld.Mathst mathst = new HelloWorld.Mathst();
+        int actual = mathst.Mathstep(a, b);
+        Assert.AreEqual(expected, actual);
+    }
+}
